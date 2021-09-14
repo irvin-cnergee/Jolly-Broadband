26461198c11b35318fc988e7acf64ee48cf57e43
Jyoti <jyoti@cnergee.com>
2021-09-14 13:52:30 +0530

    JollyBroadband

---
+++ app/src/main/java/com/cnergee/mypage/obj/AuthenticationMobile.java
@@ -0,0 +1,8 @@
+package com.cnergee.mypage.obj;
+
+public class AuthenticationMobile {
+
+	public static String CliectAccessId=  "CM000198JB";
+	public static String CliectAccessName="CliectAccessId";
+	public static String PROJECT_NUMBER="919820138387";
+}
